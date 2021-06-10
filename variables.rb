ac580b8fb443505cba39356ceb4473e77dc9f71f
gurjardivya10 <divya@webkorps.com>
2021-06-10 18:19:34 +0530

    Class variable

--- variables.rb
+++ variables.rb
@@ -20,19 +20,30 @@ class1obj.global_var_method
 class2obj.global_var_method
 
 
-#instance variable
+#instance and class variable
 class Users
+  #class variable
+  @@no_of_users = 0
   def initialize(id, name, email)
+    #instance variable
      @cust_id = id
      @cust_name = name
      @email = email
   end
+
   def print_details()
      puts "id : #@cust_id"
      puts "name : #@cust_name"
      puts "email : #@email"
   end
+
+  def total_users()
+    # @@no_of_users += 1
+    puts "Total users: #@@no_of_users"
+ end
 end
 
 userObj = Users.new('1', 'divya', 'divya@webkorps.com')
 userObj.print_details
+
+userObj.total_users
